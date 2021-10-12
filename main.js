0588aca69f8d1e9b71ef8419a42f3b3bd91119af
Polako <armatys.alex@gmail.com>
2021-10-12 11:16:22 +0200

    Animation afterLoad on section

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-console.log($('#fullpage section').attr('id'))
+$('#fullpage').fullpage({
+
+    autoScrolling: true,
+    recordHistory: true,
+    behavior: 'smooth',
+    fixedElements: 'header',
+    menu: '#menu',
+    anchors:['#home', '#aboutMe', '#why', '#contact'],
+    lockAnchors: true,
+    scrollingSpeed: 1000,
+
+    // section
+    sectionSelector: ".section",
+
+    // animation
+    afterLoad: (origin, destination, direction) => {
+        gsap.from('.text .title', { duration: 1.2, ease:"power3.out", x:-700, z:-100,},)
+        gsap.from('.decoLines span', {duration: 1, x:-500, stagger:0.2 })
+        gsap.from('.stuff', {duration: 1.8, x:-500, })
+    },
+
+
+})
+
 
