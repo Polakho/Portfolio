53268af93bb9afddfbd49dd8877c6cf8c64bd342
Polako <armatys.alex@gmail.com>
2021-10-13 10:16:32 +0200

    NavBar left

--- main.js
+++ main.js
@@ -3,23 +3,29 @@ $('#fullpage').fullpage({
     autoScrolling: true,
     recordHistory: true,
     behavior: 'smooth',
-    fixedElements: 'header',
-    menu: '#menu',
-    anchors:['#home', '#aboutMe', '#why', '#contact'],
-    lockAnchors: true,
+    fixedElements: 'header, #menu',
+    menu: '#fp-nav',
+    anchors : ["home","aboutMe","why","contact"],
     scrollingSpeed: 1000,
 
     // section
     sectionSelector: ".section",
 
     // animation
-    afterLoad: (origin, destination, direction) => {
-        gsap.from('.text .title', { duration: 1.2, ease:"power3.out", x:-700, z:-100,},)
-        gsap.from('.decoLines span', {duration: 1, x:-500, stagger:0.2 })
-        gsap.from('.stuff', {duration: 1.8, x:-500, })
+    afterRender : () => {
+
+},
+    afterLoad : () => {
+
+
+            if ($('.section').hasClass("active")){
+                gsap.from('.text .title', { duration: 1, ease:"power3.out", x:-700, z:-100,},)
+                gsap.from('.decoLines span', {duration: 1,ease:"power3.out", x:-500, stagger:0.2,})
+                gsap.from('.section h2', {duration: 1.2,ease:"power3.out", x:-500,})
+            }
     },
 
+});
 
-})
-
-
+let hash = window.location.hash;
+console.log(hash);
