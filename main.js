3877840fde11d806a0734d14d3dad33cf14d4473
Polako <armatys.alex@gmail.com>
2021-10-14 20:44:09 +0200

    navBar animation

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-$('#fullpage').fullpage({
+$(document).ready(function() {
+    $('#fullpage').fullpage({
 
-    autoScrolling: true,
-    recordHistory: true,
-    behavior: 'smooth',
-    fixedElements: 'header, #menu',
-    menu: '#fp-nav',
-    anchors : ["home","aboutMe","why","contact"],
-    scrollingSpeed: 1000,
-
-    // section
-    sectionSelector: ".section",
-
-    // animation
-    afterRender : () => {
-
-},
-    afterLoad : () => {
-
-
-            if ($('.section').hasClass("active")){
-                gsap.from('.text .title', { duration: 1, ease:"power3.out", x:-700, z:-100,},)
-                gsap.from('.decoLines span', {duration: 1,ease:"power3.out", x:-500, stagger:0.2,})
-                gsap.from('.section h2', {duration: 1.2,ease:"power3.out", x:-500,})
+        autoScrolling: true,
+        recordHistory: true,
+        behavior: 'smooth',
+        fixedElements: 'header, #menu',
+        menu: '#fp-nav',
+        navigation: true,
+        anchors : ["home","aboutMe","why","contact"],
+        scrollingSpeed: 1000,
+        sectionSelector: "section",
+        fitToSectionDelay: 1000,
+        afterRender:  () => {
+            if($('.fp-section.active')) {
+                console.log($(this).children.children.children.children(".title"))
             }
-    },
+        },
 
+        afterLoad: function(){
+
+            /** gsap.from('.text .title', { duration: 1, ease:"power3.out", x:-700, z:-100,},)
+             gsap.from('.decoLines span', {duration: 0.8,ease:"power3.out", x:-500, stagger:0.2,})
+             gsap.from('.section h2', {duration: 1.4,ease:"power3.out", x:-500,})
+             **/
+        },
+
+    });
 });
 
-let hash = window.location.hash;
-console.log(hash);
+
