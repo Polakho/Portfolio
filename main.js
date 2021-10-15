b84712e73eeb8bff183e725ec134ee19e8174100
Polako <armatys.alex@gmail.com>
2021-10-15 16:06:56 +0200

    Fixed animation on leave

--- main.js
+++ main.js
@@ -8,16 +8,57 @@ $(document).ready(function() {
         menu: '#fp-nav',
         navigation: true,
         anchors : ["home","aboutMe","why","contact"],
-        scrollingSpeed: 1000,
+        scrollingSpeed: 1e3,
         sectionSelector: "section",
         fitToSectionDelay: 1000,
+        easingcss3: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+
         afterRender:  () => {
-            if($('.fp-section.active')) {
-                console.log($(this).children.children.children.children(".title"))
+            let activeSection = $(".fp-section.active")
+            if ($(activeSection)) {
+               let tl = anime.timeline({
+                    duration: 1000,
+                    delay:100,
+                    easing: 'easeInOutCubic',
+                });
+               tl.add({
+                   targets: ".title",
+                   translateX: [-700, 0]
+               })
+                tl.add({
+                    targets: ".decoLines",
+                    translateX: [-700, 0]
+                },200)
+                tl.add({
+                    targets: ".subTitle",
+                    translateX: [-700, 0]
+                },400)
             }
         },
 
-        afterLoad: function(){
+        onLeave: function() {
+            let activeSection = $(".fp-section.active")
+            if ($(activeSection)) {
+                let tl = anime.timeline({
+                    duration: 1500,
+                    delay:50,
+                    easing: 'easeInOutCubic',
+                });
+                tl.add({
+                    targets: ".title",
+                    translateX: [-700, 0]
+                })
+                tl.add({
+                    targets: ".decoLines",
+                    translateX: [-700, 0]
+                },200)
+                tl.add({
+                    targets: ".subTitle",
+                    translateX: [-700, 0]
+                },400)
+            }
+
+
 
             /** gsap.from('.text .title', { duration: 1, ease:"power3.out", x:-700, z:-100,},)
              gsap.from('.decoLines span', {duration: 0.8,ease:"power3.out", x:-500, stagger:0.2,})
